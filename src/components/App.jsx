28131d7595dbf8868f00d6066c7ac4c419730434
Ali Tohidlou <alitohidlou@gmail.com>
2025-07-23 14:52:21 +0330

    disable button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,13 @@ const initialValues = {
   name: "",
   email: "",
   password: "",
-  bio: ""
+  bio: "",
 };
-const onSubmit = (values) => {
-  console.log(values);
+const onSubmit = (values,submitProps) => {
+  console.log(submitProps);
+  setTimeout(()=>{
+    submitProps.setSubmitting(false);
+  },10000)
 };
 const validate = yup.object({
   name: yup
@@ -21,8 +24,11 @@ const validate = yup.object({
     .required("required")
     .matches(
       /^(?=.+[A-Z])(?=.+[a-z])(?=.+[0-9])(?=.+[!@#$%^&*+]).{7,}$/,
-      "your pass must be 7 currecter with atleast oneOf !@#$%^&*+"
+      "7 currecter with atleast oneOf !@#$%^&*+"
     ),
+  bio: yup
+    .string()
+    .matches(/^[\u0600-\u06FF\s0-9A-Za-z]+$/, "you can only use word and num"),
 });
 const App = () => {
   return (
@@ -32,23 +38,30 @@ const App = () => {
         initialValues={initialValues}
         onSubmit={onSubmit}
         validationSchema={validate}
+        validateOnMount
       >
-        <Form className="registerForm">
-          <label htmlFor="name">Full Name:</label>
-          <FastField type="text" name="name" id="name" />
-          <ErrorMessage name="name" component={ErrorHandler}/>
-          <label htmlFor="email">Email:</label>
-          <FastField type="text" name="email" id="email" />
-          <ErrorMessage name="email" component={ErrorHandler}/>
-          <label htmlFor="password">Password:</label>
-          <FastField type="text" name="password" id="password" />
-          <ErrorMessage name="password">
-            {(props)=><p className="errorMsg">{props}</p>}
-          </ErrorMessage>
-          <label htmlFor="bio">bio:</label>
-          <FastField type="text" name="bio" id="bio" as="textarea" />
-          <button type="submit">Submit</button>
-        </Form>
+        {(formik) => {
+          console.log(formik);
+          return (
+            <Form className="registerForm">
+              <label htmlFor="name">Full Name:</label>
+              <FastField type="text" name="name" id="name" />
+              <ErrorMessage name="name" component={ErrorHandler} />
+              <label htmlFor="email">Email:</label>
+              <FastField type="text" name="email" id="email" />
+              <ErrorMessage name="email" component={ErrorHandler} />
+              <label htmlFor="password">Password:</label>
+              <FastField type="password" name="password" id="password" />
+              <ErrorMessage name="password">
+                {(props) => <p className="errorMsg">{props}</p>}
+              </ErrorMessage>
+              <label htmlFor="bio">bio:</label>
+              <FastField type="text" name="bio" id="bio" as="textarea" />
+              <ErrorMessage name="bio" component={ErrorHandler} />
+              <button type="submit" disabled={(!(formik.dirty && formik.isValid)||formik.isSubmitting)?true:false}>Submit</button>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
